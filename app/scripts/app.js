dcad5e1592278d597ac361efcf32aa56aa230848
Roberto García <roberto@rhizomik.net>
2016-02-02 01:54:51 +0100

    AngularJS app skeleton generated with Yeoman 1.6 and generator-angular 0.15.1

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rhizomerEyeApp
+ * @description
+ * # rhizomerEyeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rhizomerEyeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rhizomerEyeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rhizomerEyeApp
+ */
+angular.module('rhizomerEyeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
