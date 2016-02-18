77d7de0ed5f7ef45e4ca1f3c4824c50262bd5c6d
Roberto García <roberto@rhizomik.net>
2016-02-18 01:59:14 +0100

    Show list of dataset and server graphs with checkbox directive for selection

--- app/scripts/controllers/datasetdetail.js
+++ app/scripts/controllers/datasetdetail.js
@@ -11,5 +11,13 @@ angular.module('rhizomerEyeApp')
   .controller('DatasetDetailCtrl', function ($scope, $stateParams, Dataset) {
 
     $scope.dataset = Dataset.get({id : $stateParams.id});
+    $scope.datasetGraphs = Dataset.datasetGraphs({id : $stateParams.id}, function(result) {
+      $scope.newDatasetGraphs = angular.copy(result);
+    });
+    $scope.serverGraphs = Dataset.serverGraphs({id : $stateParams.id});
+
+    $scope.isDatasetGraphsChanged = function() {
+      return $scope.newDatasetGraphs.toString() !== $scope.datasetGraphs.toString();
+    };
 
   });
--- app/scripts/services/dataset.js
+++ app/scripts/services/dataset.js
@@ -11,6 +11,8 @@ angular.module('rhizomerEyeApp')
   .service('Dataset', function ($resource, environment) {
     return $resource(environment.api+'/datasets/:id', {},
       {
-        'update': { method: 'PUT' }
+        'update': { method: 'PUT' },
+        'datasetGraphs': { method: 'GET', url: environment.api+'/datasets/:id/graphs', isArray: true },
+        'serverGraphs': { method: 'GET', url: environment.api+'/datasets/:id/server/graphs', isArray: true }
       });
   });
