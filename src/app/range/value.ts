883de887818e9e16d39de81d10c16860d7dd094b
Roberto García <roberto@rhizomik.net>
2019-02-10 21:01:05 +0100

    Detect active filters taking into account facet level filter with null value

--- src/app/range/value.ts
+++ src/app/range/value.ts
@@ -20,6 +20,6 @@ export class Value {
       }
     }
     this.selected = filters.filter((filter: Filter) =>
-      (filter.facet.id === facet.id && filter.value.value === this.value)).length > 0;
+      (filter.facet.id === facet.id && filter.value && filter.value.value === this.value)).length > 0;
   }
 }
