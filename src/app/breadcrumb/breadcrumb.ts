e3fce50909fd4a019880b23a4045039a58326147
Roberto García <roberto@rhizomik.net>
2019-02-10 20:59:42 +0100

    Do not capitalize first letter in breadcrumbs steps

--- src/app/breadcrumb/breadcrumb.ts
+++ src/app/breadcrumb/breadcrumb.ts
@@ -3,7 +3,7 @@ export class Breadcrumb {
   uri: string;
 
   constructor(step: string, url: string) {
-    this.name = decodeURI(step.charAt(0).toUpperCase() + step.slice(1));
+    this.name = decodeURI(step);
     this.uri = url.substring(0, url.indexOf(step)) + step;
   }
 }
