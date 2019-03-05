caa3b07159eab0ea0aca3df008c206740a350be6
Roberto García <roberto@rhizomik.net>
2019-03-05 20:17:45 +0100

    Add method to dataset service to fetch resource description

--- src/app/dataset/dataset.service.ts
+++ src/app/dataset/dataset.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RestService } from '../shared/rest.service';
 import { Dataset } from './dataset';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Class } from '../class/class';
@@ -30,5 +30,12 @@ export class DatasetService extends RestService<Dataset> {
   clearClasses(did: string): Observable<Class[]> {
     const body = JSON.stringify([]);
     return this.http.put<Class[]>(`${environment.API}/datasets/${did}/classes`, body, this.getHttpOptions());
+  }
+
+  describeDatasetResource(did: string, resource: string): Observable<string[]> {
+    let params = new HttpParams();
+    params = params.append('uri', resource);
+    return this.http.get<any>(
+      `${environment.API}/datasets/${did}/describe`, {params: params});
   }
 }
