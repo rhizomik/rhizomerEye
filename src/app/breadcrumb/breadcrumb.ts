e6c3e11678d50554da7eb2a40da0b7ee796351e3
Roberto García <roberto@rhizomik.net>
2019-03-22 00:16:28 +0100

    Fix breadcrumb for URI describe

--- src/app/breadcrumb/breadcrumb.ts
+++ src/app/breadcrumb/breadcrumb.ts
@@ -3,7 +3,10 @@ export class Breadcrumb {
   uri: string;
 
   constructor(step: string, url: string) {
-    this.name = decodeURI(step);
+    this.name = decodeURIComponent(step);
+    if (this.name.startsWith('describe?uri=')) {
+      this.name = this.name.substring('describe?uri='.length);
+    }
     this.uri = url.substring(0, url.indexOf(step)) + step;
   }
 }
