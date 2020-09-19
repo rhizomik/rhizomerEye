6934234b7cd88f49884f6527b4c85572b0a2c7a1
Roberto García <roberto@rhizomik.net>
2020-09-19 20:57:31 +0200

    Load instances and labels in parallel, process labels first to pass them to new instances Descriptions

--- src/app/facet/list-facet/list-facet.component.ts
+++ src/app/facet/list-facet/list-facet.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subject} from 'rxjs';
+import { forkJoin, Subject } from 'rxjs';
 import { BreadcrumbService } from '../../breadcrumb/breadcrumb.service';
 import { ClassService } from '../../class/class.service';
 import { FacetService } from '../facet.service';
@@ -82,42 +82,42 @@ export class ListFacetComponent implements OnInit, OnDestroy {
   }
 
   loadInstances(datasetId: string, classId: string, filters: Filter[], page: number) {
-    this.classService.getInstances(datasetId, classId, filters, page, this.pageSize).subscribe(
-      instances => {
-        if (instances['@graph']) {
-          instances['@graph']
-            .filter(instance => (<string>instance['@id']).startsWith('_:'))
-            .map(instance => this.anonResources.set(instance['@id'],
-              new Description(instance, instances['@context'], this.labels)));
-          instances['@graph']
-            .map(instance => Object.entries(instance)
-              .forEach(([key, value]) => {
-                if (key.includes('label')) {
-                  this.labels.set(UriUtils.expandUri(instance['@id'], instances['@context']), <string>value);
-                }
-              }));
-          this.resources = instances['@graph']
-            .filter(instance => instance['@type'] &&
-              Description.isOfType(instance['@type'], this.datasetClass.uri, instances['@context']) )
-            .map(instance => new Description(instance, instances['@context'], this.labels));
-        } else if (instances['@type']) {
-          this.resources = [new Description(instances, instances['@context'], this.labels)];
-        } else {
-          this.resources = [];
-        }
-      });
-    this.classService.getInstancesLabels(datasetId, classId, filters, page, this.pageSize).subscribe(
-      labels => {
-        if (labels['@graph']) {
-          labels['@graph']
-            .map(instance => Object.entries(instance)
-              .forEach(([key, value]) => {
-                if (key.includes('label')) {
-                  this.labels.set(UriUtils.expandUri(instance['@id'], labels['@context']), <string>value);
-                }
-              }));
-        }
-      });
+    forkJoin([
+      this.classService.getInstances(datasetId, classId, filters, page, this.pageSize),
+      this.classService.getInstancesLabels(datasetId, classId, filters, page, this.pageSize) ])
+      .subscribe(
+        ([instances, labels]) => {
+          if (labels['@graph']) {
+            labels['@graph']
+              .map(instance => Object.entries(instance)
+                .forEach(([key, value]) => {
+                  if (key.includes('label')) {
+                    this.labels.set(UriUtils.expandUri(instance['@id'], labels['@context']), <string>value);
+                  }
+                }));
+          }
+          if (instances['@graph']) {
+            instances['@graph']
+              .filter(instance => (<string>instance['@id']).startsWith('_:'))
+              .map(instance => this.anonResources.set(instance['@id'],
+                new Description(instance, instances['@context'], this.labels)));
+            instances['@graph']
+              .map(instance => Object.entries(instance)
+                .forEach(([key, value]) => {
+                  if (key.includes('label')) {
+                    this.labels.set(UriUtils.expandUri(instance['@id'], instances['@context']), <string>value);
+                  }
+                }));
+            this.resources = instances['@graph']
+              .filter(instance => instance['@type'] &&
+                Description.isOfType(instance['@type'], this.datasetClass.uri, instances['@context']) )
+              .map(instance => new Description(instance, instances['@context'], this.labels));
+          } else if (instances['@type']) {
+            this.resources = [new Description(instances, instances['@context'], this.labels)];
+          } else {
+            this.resources = [];
+          }
+        });
   }
 
   goToPage(page: number) {
